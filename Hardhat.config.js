8c204d6f165fed2900df7dedffca897aa3ec1443
Edmund Berkmann <eddieberkmann@gmail.com>
2023-04-09 03:06:15 +0100

    HardHat.config.js Completed
    Ready to test, then move on to the Solidity Contracts

---
+++ Hardhat.config.js
@@ -0,0 +1,38 @@
+require("@nomiclabs/hardhat-ethers");
+require("hardhat-deploy");
+
+const ALCHEMY_API_KEY = "jhT3QEajr6JISNWO7O9SZi0fncOm94Vq";
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.19", // Change this to 0.8.19 for your project
+  networks: {
+    mumbai: {
+      url: `https://polygon-mumbai.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: {
+        mnemonic: process.env.MNEMONIC || "",
+      },
+      chainId: 137,
+    },
+    // Add ZkEVM network configuration here when needed
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  },
+  external: {
+    contracts: [],
+  },
+  ipfs: {
+    gateway: "https://ipfs.io/ipfs",
+  },
+};
