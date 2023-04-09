b1c4aae44d98cbb9957738145e0cf454f6cce883
Edmund Berkmann <eddieberkmann@gmail.com>
2023-04-09 16:01:36 +0100

    Token Renamed
    Token Renamed to "Steelo Token"

--- Contracts/SteeloToken.sol
+++ Contracts/SteeloToken.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.8.19;
 
 import "node_modules/@openzeppelin/contracts/token/ERC1155/ERC1155.sol"; 
 
-contract MyERC1155 is ERC1155 {
+contract SteeloToken is ERC1155 {
     constructor() ERC1155("https://myapi.com/api/token/{id}.json") {
         // Your custom constructor code here
     }
