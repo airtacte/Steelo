540b9693c2bf7bad68d65c9498689e5a9cad30c0
Edmund Berkmann <eddieberkmann@gmail.com>
2023-04-09 03:57:38 +0100

    Update SteeloToken.sol

--- Contracts/SteeloToken.sol
+++ Contracts/SteeloToken.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.8.19;
 
-import "@openzeppelin\contracts\token\ERC1155\ERC1155.sol";
+import "node_modules/@openzeppelin/contracts/token/ERC1155/ERC1155.sol"; 
 
 contract MyERC1155 is ERC1155 {
     constructor() ERC1155("https://myapi.com/api/token/{id}.json") {
