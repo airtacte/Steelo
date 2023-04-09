7e57d1d3089c744bb3db838d7283f6eea1918666
Edmund Berkmann <eddieberkmann@gmail.com>
2023-04-09 03:41:30 +0100

    OpenZeppelin Template Pull
    ERC1155.sol template being pulled for future customisation

---
+++ Contracts/SteeloToken.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+
+contract MyERC1155 is ERC1155 {
+    constructor() ERC1155("https://myapi.com/api/token/{id}.json") {
+        // Your custom constructor code here
+    }
+
+    // Your custom contract code here
+}
