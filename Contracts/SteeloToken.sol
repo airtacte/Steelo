bc4389aa92946ffa776fab5a3c4c0ea6d30340ac
airtacte <eddieberkmann@gmail.com>
2023-04-15 13:50:17 +0100

    SteeloToken 0.1.3
    By making these changes, the SteeloToken contract becomes more flexible and maintainable. The contract owner can now modify the maximum allowed tokens for a creator, and update the base URI when needed. Additionally, the royalty values are now stored in a mapping, allowing for dynamic royalty configurations.
    
    With these improvements, the SteeloToken smart contract is more adaptable to changing requirements and can be better managed by the contract owner

--- Contracts/SteeloToken.sol
+++ Contracts/SteeloToken.sol
@@ -19,18 +19,14 @@ contract SteeloToken is ERC1155Upgradeable, OwnableUpgradeable, PausableUpgradea
     using Strings for uint256;
 
     // Constants
-    uint256 private constant MAX_CREATOR_TOKENS = 500;
     uint256 private constant INITIAL_CAP = 500;
     uint256 private constant TRANSACTION_MULTIPLIER = 2;
+    uint256 private constant PRE_ORDER_MINIMUM_SOLD = 125; // 50% of 250
 
-    // Royalties
-    uint256 private constant TOKEN_CREATOR_ROYALTY = 50; // 5%
-    uint256 private constant STEELO_ROYALTY = 25; // 2.5%
-    uint256 private constant HOLDER_ROYALTY = 25; // 2.5%
-
-    // State variables
-    Counters.Counter private _currentTokenID;
+    // State Variables
+    uint256 private _maxCreatorTokens;
     string private _baseURI;
+    Counters.Counter private _currentTokenID;
     mapping(uint256 => uint256) private _totalSupply;
     mapping(uint256 => uint256) private _transactionCount;
     mapping(uint256 => address) private _creator;
@@ -42,20 +38,32 @@ contract SteeloToken is ERC1155Upgradeable, OwnableUpgradeable, PausableUpgradea
     uint256 private _lastSnapshotTimestamp;
     mapping(uint256 => mapping(address => uint256)) private _snapshotBalances;
     uint256 private _snapshotCounter;
+    mapping(address => bool) private _hasCreatedToken;
+
+    // Royalties
+    uint256 private constant TOKEN_CREATOR_ROYALTY = 50; // 5%
+    uint256 private constant STEELO_ROYALTY = 25; // 2.5%
+    uint256 private constant HOLDER_ROYALTY = 25; // 2.5%
 
     // Events
     event TokenMinted(uint256 indexed tokenId, address indexed creator, uint256 totalSupply);
     event AnnualTokenIncrease(uint256 indexed tokenId);
     event RoyaltyUpdated(uint256 indexed tokenId, address indexed user, uint256 updatedRoyaltyAmount);
+    event TokenBurned(uint256 indexed tokenId, address indexed owner);
 
     // Modifiers
     modifier dailySnapshot() {
-    if (block.timestamp >= _lastSnapshotTimestamp.add(1 days)) {
-        _takeSnapshot();
-        _lastSnapshotTimestamp = block.timestamp;
+        if (block.timestamp >= _lastSnapshotTimestamp.add(1 days)) {
+            _takeSnapshot();
+            _lastSnapshotTimestamp = block.timestamp;
+        }
+        _;
     }
-    _;
-}
+
+    modifier canCreateToken(address creator) {
+        require(!_hasCreatedToken[creator], "SteeloToken: Creator has already created a token.");
+        _;
+    }
 
     // Add the following contract addresses
     address private constant GNOSIS_SAFE_MASTER_COPY = 0x34CfAC646f301356fAa8B21e94227e3583Fe3F5F; // Mainnet Gnosis Safe Master Copy address. Update it based on your network.
@@ -65,32 +73,106 @@ contract SteeloToken is ERC1155Upgradeable, OwnableUpgradeable, PausableUpgradea
 
         // Initialize With Safe-Global's MultiSig Wallet
         function initializeWithSafe(string memory baseURI, address safeAddress) public initializer {
-        __ERC1155_init("https://myapi.com/api/token/{id}.json");
-        __Ownable_init_unchained(safeAddress);
-        __Pausable_init_unchained();
+            __ERC1155_init("https://myapi.com/api/token/{id}.json");
+            __Ownable_init_unchained(safeAddress);
+            __Pausable_init_unchained();
 
-        _baseURI = baseURI;
+            _baseURI = baseURI;
 
-        // Gnosis Safe multisig setup
-        GnosisSafeProxyFactory proxyFactory = GnosisSafeProxyFactory(GNOSIS_SAFE_PROXY_FACTORY);
-        GnosisSafeProxy proxy = GnosisSafeProxy(proxyFactory.createProxy(GNOSIS_SAFE_MASTER_COPY, ""));
-        ISafe safe = ISafe(address(proxy));
-        
-        // Configure the Gnosis Safe with the given `safeAddress`
-        safe.setup(
-            new address[](1){safeAddress}, // The Safe owners
-            1,                             // The number of required confirmations
-            address(0),                    // Address of the module that checks signatures
-            bytes(""),                     // Data for the module that checks signatures
-            address(0),                    // Address of the fallback handler
-            address(0),                    // Address of the token payment receiver
-            0,                             // Value of the token payment
-            address(0)                     // Address of the token to pay
-        );
-        
-        // Transfer ownership of the contract to the Gnosis Safe
-        transferOwnership(address(safe));
-    }
+            // Gnosis Safe multisig setup
+            GnosisSafeProxyFactory proxyFactory = GnosisSafeProxyFactory(GNOSIS_SAFE_PROXY_FACTORY);
+            GnosisSafeProxy proxy = GnosisSafeProxy(proxyFactory.createProxy(GNOSIS_SAFE_MASTER_COPY, ""));
+            ISafe safe = ISafe(address(proxy));
+            
+            // Configure the Gnosis Safe with the given `safeAddress`
+            safe.setup(
+                new address[](1){safeAddress}, // The Safe owners
+                1,                             // The number of required confirmations
+                address(0),                    // Address of the module that checks signatures
+                bytes(""),                     // Data for the module that checks signatures
+                address(0),                    // Address of the fallback handler
+                address(0),                    // Address of the token payment receiver
+                0,                             // Value of the token payment
+                address(0)                     // Address of the token to pay
+            );
+            
+            // Transfer ownership of the contract to the Gnosis Safe
+            transferOwnership(address(safe));
+        }
+
+        // Set the maximum allowed tokens for a creator
+        function setMaxCreatorTokens(uint256 maxTokens) public onlyOwner {
+            _maxCreatorTokens = maxTokens;
+        }
+
+        // Get the maximum allowed tokens for a creator
+        function getMaxCreatorTokens() public view returns (uint256) {
+            return _maxCreatorTokens;
+        }
+
+        // Set the base URI
+        function setBaseURI(string memory newBaseURI) public onlyOwner {
+            _baseURI = newBaseURI;
+            emit BaseURIUpdated(newBaseURI);
+        }
+
+        // Get the base URI
+        function baseURI() public view returns (string memory) {
+            return _baseURI;
+        }
+
+        // Override _beforeTokenTransfer() from ERC1155Upgradeable.sol
+        function _beforeTokenTransfer(address operator, address from, address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
+            internal
+            whenNotPaused
+            override
+        {
+            super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
+        }
+
+        // Create token
+        function createToken() public onlyOwner {
+            require(_creator[msg.sender] == false, "SteeloToken: Creator has already created a token.");
+            _creator[msg.sender] = true;
+
+            _currentTokenID.increment();
+            uint256 newTokenId = _currentTokenID.current();
+
+            emit TokenCreated(newTokenId, msg.sender);
+        }
+
+        // Pre-order function
+        function preOrder(uint256 tokenId, uint256 amount) public payable onlyOwner {
+            require(_totalSupply[tokenId] == 0, "SteeloToken: Pre-order has already been completed.");
+
+            // Check for pre-order eligibility
+            uint256 minTokenAmount = MAX_CREATOR_TOKENS.div(2);
+            require(amount >= minTokenAmount, "SteeloToken: Minimum pre-order amount not reached.");
+
+            // Mint pre-ordered tokens
+            _mint(msg.sender, tokenId, amount, "");
+            _totalSupply[tokenId] = amount;
+
+            emit PreOrderMinted(tokenId, msg.sender, amount);
+        }
+
+        // Launch token function
+        function launchToken(uint256 tokenId, uint256 amount) public payable onlyOwner {
+            require(_totalSupply[tokenId] > 0, "SteeloToken: Pre-order must be completed first.");
+            require(_totalSupply[tokenId].add(amount) <= MAX_CREATOR_TOKENS, "SteeloToken: Maximum cap exceeded.");
+
+            // Mint launched tokens
+            _mint(msg.sender, tokenId, amount, "");
+            _totalSupply[tokenId] = _totalSupply[tokenId].add(amount);
+
+            emit TokenLaunched(tokenId, msg.sender, _totalSupply[tokenId]);
+        }
+
+        // Burn token
+        function burn(uint256 tokenId, uint256 amount) public onlyOwner {
+            require(amount <= balanceOf(msg.sender, tokenId), "SteeloToken: Insufficient balance to burn.");
+            _burn(msg.sender, tokenId, amount);
+        }
 
         // Snapshot function
         function _takeSnapshot() internal {
@@ -110,7 +192,6 @@ contract SteeloToken is ERC1155Upgradeable, OwnableUpgradeable, PausableUpgradea
         ) public dailySnapshot {
             _transferWithRoyalty(from, to, tokenId, amount);
         }
-
 
         // Minting function
         function mint(address to, uint256 tokenId, uint256 amount, bytes memory data) public onlyOwner dailySnapshot {
@@ -148,76 +229,76 @@ contract SteeloToken is ERC1155Upgradeable, OwnableUpgradeable, PausableUpgradea
                 return string(abi.encodePacked(_baseURI, tokenId.toString(), ".json"));
     }
 
-    function totalSupply(uint256 tokenId) public view returns (uint256) {
-        return _totalSupply[tokenId];
-    }
-
-    function tokenCreator(uint256 tokenId) public view returns (address) {
-        return _creator[tokenId];
-    }
-
-    function royalty(uint256 tokenId) public view returns (uint256) {
-        return _royalty[tokenId];
-    }
-
-    function transactionCount(uint256 tokenId) public view returns (uint256) {
-        return _transactionCount[tokenId];
-    }
-
-    function mintedInLastYear(uint256 tokenId) public view returns (uint256) {
-        return _mintedInLastYear[tokenId];
-    }
-
-    function lastMintTime(uint256 tokenId) public view returns (uint256) {
-        return _lastMintTime[tokenId];
-    }
-
-    function _takeSnapshot(/* parameters */) internal {
-        // Snapshot logic
-    }
-
-    function expansionEligible(uint256 tokenId) public view returns (bool) {
-        return _transactionCount[tokenId] >= _totalSupply[tokenId].mul(2);
-    }
-
-    // Additional functions for token management, like Semi-fungible tokens, Hooks, Upgrading, batchTransfer, batchBalance, safeBatchTransferFrom, etc.
-    // Optional functions such as approve, allowance, isApprovedForAll, setApprovalForAll
-
-    // Annual token increase eligibility check
-    function checkAnnualTokenIncrease(uint256 tokenId) public view returns (bool) {
-        return (block.timestamp >= _lastMintTime[tokenId].add(365 days)) &&
-            (_transactionCount[tokenId] >= _totalSupply[tokenId].mul(TRANSACTION_MULTIPLIER));
-    }
-
-    // Function to initiate the annual token increase process
-    function initiateAnnualTokenIncrease(uint256 tokenId) external {
-        require(checkAnnualTokenIncrease(tokenId), "SteeloToken: Token not eligible for increase");
-        _lastMintTime[tokenId] = block.timestamp;
-
-        emit AnnualTokenIncrease(tokenId);
-    }
-
-    // Custom transfer function for royalty distribution
-    function transferWithRoyalty(
-        address from,
-        address to,
-        uint256 tokenId,
-        uint256 amount
-    ) public dailySnapshot {
-        _transferWithRoyalty(from, to, tokenId, amount);
-    }
-
-        function _distributeCommunityRoyalty(
-            uint256 tokenId,
-            uint256 communityRoyalty
-        ) internal {
-            uint256 totalSnapshotSupply = _snapshotBalances[_snapshotCounter][tokenId];
-            uint256 royaltyPerToken = communityRoyalty.div(totalSnapshotSupply);
-
-            for (uint256 i = 1; i <= _currentTokenID.current(); i++) {
-                _undistributedRoyalties[i][tokenId] = _undistributedRoyalties[i][tokenId].add(royaltyPerToken);
-            }
+        function totalSupply(uint256 tokenId) public view returns (uint256) {
+            return _totalSupply[tokenId];
         }
+
+        function tokenCreator(uint256 tokenId) public view returns (address) {
+            return _creator[tokenId];
+        }
+
+        function royalty(uint256 tokenId) public view returns (uint256) {
+            return _royalty[tokenId];
+        }
+
+        function transactionCount(uint256 tokenId) public view returns (uint256) {
+            return _transactionCount[tokenId];
+        }
+
+        function mintedInLastYear(uint256 tokenId) public view returns (uint256) {
+            return _mintedInLastYear[tokenId];
+        }
+
+        function lastMintTime(uint256 tokenId) public view returns (uint256) {
+            return _lastMintTime[tokenId];
+        }
+
+        function _takeSnapshot(/* parameters */) internal {
+            // Snapshot logic
+        }
+
+        function expansionEligible(uint256 tokenId) public view returns (bool) {
+            return _transactionCount[tokenId] >= _totalSupply[tokenId].mul(2);
+        }
+
+        // Additional functions for token management, like Semi-fungible tokens, Hooks, Upgrading, batchTransfer, batchBalance, safeBatchTransferFrom, etc.
+        // Optional functions such as approve, allowance, isApprovedForAll, setApprovalForAll
+
+        // Annual token increase eligibility check
+        function checkAnnualTokenIncrease(uint256 tokenId) public view returns (bool) {
+            return (block.timestamp >= _lastMintTime[tokenId].add(365 days)) &&
+                (_transactionCount[tokenId] >= _totalSupply[tokenId].mul(TRANSACTION_MULTIPLIER));
+        }
+
+        // Function to initiate the annual token increase process
+        function initiateAnnualTokenIncrease(uint256 tokenId) external {
+            require(checkAnnualTokenIncrease(tokenId), "SteeloToken: Token not eligible for increase");
+            _lastMintTime[tokenId] = block.timestamp;
+
+            emit AnnualTokenIncrease(tokenId);
+        }
+
+        // Custom transfer function for royalty distribution
+        function transferWithRoyalty(
+            address from,
+            address to,
+            uint256 tokenId,
+            uint256 amount
+        ) public dailySnapshot {
+            _transferWithRoyalty(from, to, tokenId, amount);
+        }
+
+            function _distributeCommunityRoyalty(
+                uint256 tokenId,
+                uint256 communityRoyalty
+            ) internal {
+                uint256 totalSnapshotSupply = _snapshotBalances[_snapshotCounter][tokenId];
+                uint256 royaltyPerToken = communityRoyalty.div(totalSnapshotSupply);
+
+                for (uint256 i = 1; i <= _currentTokenID.current(); i++) {
+                    _undistributedRoyalties[i][tokenId] = _undistributedRoyalties[i][tokenId].add(royaltyPerToken);
+                }
+            }
 
         function claimRoyalty(uint256 tokenId) external {
             uint256 userRoyalty = _undistributedRoyalties[tokenId][msg.sender];
@@ -266,6 +347,4 @@ contract SteeloToken is ERC1155Upgradeable, OwnableUpgradeable, PausableUpgradea
             // Emit an event to notify the dApp that the user's royalty has been updated
             emit RoyaltyUpdated(tokenId, msg.sender, updatedRoyaltyAmount);
         }
-
-
 }
