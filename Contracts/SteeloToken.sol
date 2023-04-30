8d86a0247bc216001c65dc79dd59b724fa7dccc1
Edmund Berkmann <eddieberkmann@gmail.com>
2023-04-30 01:15:09 +0100

    Steelo 0.0.1b
    Little line fix before feeding it to the GPT-Monster

--- Contracts/SteeloToken.sol
+++ Contracts/SteeloToken.sol
@@ -2,8 +2,9 @@
 // contracts/Diamond.sol
 
 pragma solidity ^0.8.19;
+import "./interfaces/IERC2535.sol";
 
-interface IDiamond {
+interface IERC2535 {
     enum FacetCutAction {Add, Replace, Remove}
     // Add=0, Replace=1, Remove=2
  
@@ -27,7 +28,7 @@ import "./CreatorToken.sol";
 import "./Royalties.sol";
 import "./interfaces/IERC2535.sol";
 
-contract SteeloToken is Initializable, IERC2535, ERC1155Upgradeable, OwnableUpgradeable, PausableUpgradeable, IDiamondCut, IDiamondLoupe {
+contract SteeloToken is Initializable, IERC2535, ERC1155Upgradeable, OwnableUpgradeable, PausableUpgradeable, IDiamondCut, IDiamondLoupe, IDiamond {
     bytes32 constant DIAMOND_STORAGE_POSITION = keccak256("diamond.standard.storage");
     using SafeMath for uint256;
     using Address for address;
@@ -78,8 +79,71 @@ constructor() {
         }
     }
 
-    function diamondCut(bytes[] calldata _diamondCut) external {
-        // Implement diamondCut logic here
+    function diamondCut(IDiamondCut.FacetCut[] calldata _diamondCut, address _init, bytes calldata _calldata) external override {
+        DiamondStorage storage ds = diamondStorage();
+        require(msg.sender == ds.facets[bytes4(keccak256("DiamondCut(address,IDiamondCut.FacetCut[],bytes)"))], "Diamond: Wrong facet address for diamondCut");
+        for (uint256 i = 0; i < _diamondCut.length; i++) {
+            IDiamondCut.FacetCut memory cut = _diamondCut[i];
+            if (cut.action == IDiamondCut.FacetCutAction.Add) {
+                require(cut.facetAddress != address(0), "Diamond: add facet to address 0");
+                ds.facets[cut.functionSelectors[0]] = cut.facetAddress;
+                for (uint256 j = 0; j < cut.functionSelectors.length; j++) {
+                    bytes4 selector = cut.functionSelectors[j];
+                    ds.supportedInterfaces[cut.facetAddress][selector] = true;
+                    emit DiamondCut(_diamondCut, _init, _calldata);
+                }
+            } else if (cut.action == IDiamondCut.FacetCutAction.Replace) {
+                require(cut.facetAddress != address(0), "Diamond: replace facet with address 0");
+                for (uint256 j = 0; j < cut.functionSelectors.length; j++) {
+                    bytes4 selector = cut.functionSelectors[j];
+                    ds.facets[selector] = cut.facetAddress;
+                    ds.supportedInterfaces[cut.facetAddress][selector] = true;
+                    emit DiamondCut(_diamondCut, _init, _calldata);
+                }
+            } else if (cut.action == IDiamondCut.FacetCutAction.Remove) {
+                require(cut.facetAddress == address(0), "Diamond: remove facet address must be 0");
+                for (uint256 j = 0; j < cut.functionSelectors.length; j++) {
+                    bytes4 selector = cut.functionSelectors[j];
+                    ds.supportedInterfaces[ds.facets[selector]][selector] = false;
+                    delete ds.facets[selector];
+                    emit DiamondCut(_diamondCut, _init, _calldata);
+                }
+            }
+        }
+    }
+
+
+    function facets(bytes4 _interfaceId) external view override returns (address[] memory) {
+        DiamondStorage storage ds = diamondStorage();
+        address[] memory facetAddresses = new address[](1);
+        facetAddresses[0] = ds.facets[_interfaceId];
+        return facetAddresses;
+    }
+
+    function facetFunctionSelectors(address _facet) external view override returns (bytes4[] memory) {
+        DiamondStorage storage ds = diamondStorage();
+        uint256 numSelectors = 0;
+        bytes4[] memory selectors = new bytes4[](numSelectors);
+        for (bytes4 selector : selectors) {
+            if (ds.supportedInterfaces[_facet][selector]) {
+                selectors[numSelectors] = selector;
+                numSelectors++;
+            }
+        }
+        return selectors;
+    }
+
+    function facetAddresses() external view override returns (address[] memory) {
+        DiamondStorage storage ds = diamondStorage();
+        uint256 numFacets = 0;
+        address[] memory facetAddresses = new address[](numFacets);
+        for (address facet : facetAddresses) {
+            if (ds.facets[bytes4(keccak256(abi.encodePacked(facet))))] == facet) {
+                facetAddresses[numFacets] = facet;
+                numFacets++;
+            }
+        }
+        return facetAddresses;
     }
 
     function supportsInterface(bytes4 _interfaceID) external view returns (bool) {
