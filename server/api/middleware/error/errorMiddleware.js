3fd0b39fe6a5a0a80c309b2487dd3812d0cf5d04
Edmund Berkmann <eddieberkmann@gmail.com>
2024-02-21 00:28:31 +0000

    Mobile update
    Update errorMiddleware.js

--- server/api/middleware/error/errorMiddleware.js
+++ server/api/middleware/error/errorMiddleware.js
@@ -1,4 +1,24 @@
-module.exports = function (err, req, res, next) {
-    console.error(err.message, err);
-    res.status(500).send('Something went wrong.');
+module.exports = (err, req, res, next) => {
+    console.error(err); // Log the error for debugging purposes
+
+    // Define the error structure
+    const errorResponse = {
+        status: 'error',
+        message: 'Internal Server Error',
+        // Optionally include more detailed information in development mode
+        ...(process.env.NODE_ENV === 'development' && { error: err.message, stack: err.stack })
+    };
+
+    // Check for specific types of errors to customize the response
+    if (err.name === 'ValidationError') {
+        // Example for handling validation errors
+        errorResponse.message = err.message;
+        errorResponse.details = err.details; // Assuming err.details contains validation error details
+        return res.status(400).json(errorResponse);
+    }
+
+    // Add more conditions as needed to handle different types of errors
+
+    // For unhandled errors, send a generic 500 Internal Server Error response
+    return res.status(500).json(errorResponse);
 };
